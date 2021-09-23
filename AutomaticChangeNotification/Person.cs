cf18e8ef2064e9a6b4c81d2560b5e79d952965e2
Toan Phan <toanphan1302@gmail.com>
2021-09-23 08:45:54 -0700

    9/23 8:45 AM

---
+++ AutomaticChangeNotification/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticChangeNotification
+{
+    class Person
+    {
+        private String _firstName;
+        public String FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value;
+                OnFirstNameChanged();
+                }
+        }
+        public event EventHandler FirstNameChanged;
+        private void OnFirstNameChanged()
+        {
+            if (FirstNameChanged != null)
+                FirstNameChanged(this, EventArgs.Empty);
+        }
+    }
+}
